e1965df43bc49935fbadf897f443608718d92f2f
Sydney Cohen <chnsydney@gmail.com>
2023-02-21 14:14:32 +0100

    react app url back

--- src/env.js
+++ src/env.js
@@ -1,10 +1,11 @@
 export const URLFRONT =
   process.env.NODE_ENV === 'production' ? 'https://dx.axolo.co' : 'http://localhost:3000/'
 
+console.log('process.env', process.env)
 export const URLBACK =
   process.env.NODE_ENV === 'production'
     ? 'https://dxapi.axolo.co/api/'
-    : `${process.env.URL_BACK}/api/`
+    : `${process.REACT_APP_URLBACK.URL_BACK}/api/`
 
 export const REACT_APP_DX_BOT_SLACK_HOOK_URL =
   'https://hooks.slack.com/services/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'
